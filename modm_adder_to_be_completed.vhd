207d461dac92e1d37701dbd78ec06245423e54aa
abrahamvee <49734112+abrahamvee@users.noreply.github.com>
2019-11-27 21:54:25 +0100

    Add files via upload

---
+++ modm_adder_to_be_completed.vhd
@@ -0,0 +1,51 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_arith.all;
+use IEEE.std_logic_unsigned.all;
+
+
+package dar_modm_multiplier_parameters is
+  constant K: integer := 8;
+  constant logK: integer := 3;
+  constant integer_M: integer := 239;
+  constant M: std_logic_vector(k-1 downto 0) := conv_std_logic_vector(integer_M, K);
+  constant minus_M: std_logic_vector(K-1 downto 0) := conv_std_logic_vector(2**k - integer_M, K);
+  constant ZERO: std_logic_vector(logK-1 downto 0) := (others => '0');
+end dar_modm_multiplier_parameters;
+
+
+library ieee; 
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+use IEEE.numeric_std.ALL;
+use work.dar_modm_multiplier_parameters.all;
+
+entity modm_adder_to_be_completed is
+port (
+  x, y: in std_logic_vector(K-1 downto 0);
+  z: out std_logic_vector(K-1 downto 0)
+);
+end modm_adder_to_be_completed;
+
+architecture rtl of modm_adder_to_be_completed is
+  signal long_x, sum1, long_z1, sum2: std_logic_vector(K downto 0);
+  signal c1, c2, sel: std_logic;
+  signal z1, z2: std_logic_vector(K-1 downto 0);
+
+begin
+  long_x <= '0' & x;
+  sum1 <= long_x + y;
+  c1 <= sum1(K);
+  z1 <= sum1(K-1 downto 0);
+ -- sum2 <= z1 mod std_logic_vector(to_unsigned((2**k),k)) + std_logic_vector(to_unsigned((2**k),k)) - m;
+-- sum2 <= z1 mod std_logic_vector(to_unsigned((2**k),k)) + std_logic_vector(to_unsigned((2**k),k)) - m
+   
+  long_z1 <= '0'& z1;
+  
+  sum2 <= long_z1 +  minus_M;
+  c2 <= sum2(k); 
+  z2 <= sum2(k-1 downto 0);
+  sel <= c1 or c2;
+  with sel select z <= z1 when '0', z2 when others;
+end rtl;
--- tb_modm_addition.vhd
+++ tb_modm_addition.vhd
@@ -4,69 +4,76 @@ use IEEE.numeric_std.ALL;
 
 ENTITY tb_modm_addition IS
 END tb_modm_addition;
- 
 ARCHITECTURE behavior OF tb_modm_addition IS 
  
     -- Component Declaration for the Unit Under Test (UUT)
  
-    COMPONENT modm_addition
+    COMPONENT dar_modm_multiplier
     PORT(
          x : IN  std_logic_vector(7 downto 0);
          y : IN  std_logic_vector(7 downto 0);
-         m : IN  std_logic_vector(7 downto 0);
-         z : OUT  std_logic_vector(7 downto 0)
+          clk, reset, start: in std_logic;
+         --m : IN  std_logic_vector(7 downto 0);
+         z : OUT  std_logic_vector(7 downto 0);
+         done: out std_logic
         );
-        
     END COMPONENT;
     
-    COMPONENT modm_adder
-    PORT(
-         x : IN  std_logic_vector(7 downto 0);
-         y : IN  std_logic_vector(7 downto 0);
-         z: OUT  std_logic_vector(7 downto 0)
-        );
-    END COMPONENT;
 
    --Inputs
    signal x : std_logic_vector(7 downto 0) := (others => '0');
    signal y : std_logic_vector(7 downto 0) := (others => '0');
-   signal m : std_logic_vector(7 downto 0) := (others => '0');
-   signal z : std_logic_vector(7 downto 0) := (others => '0');
+   signal clk: std_logic := '0';
+   signal start: std_logic := '0';
+   signal reset: std_logic := '0';
    
- 
+   --signal m : std_logic_vector(7 downto 0) := (others => '0');
+   signal z : std_logic_vector(7 downto 0) := (others => '0');
+   signal done: std_logic := '0';
+ constant Clk_period : time := 20 ns; 
+
 BEGIN
- 
+
 	-- Instantiate the Unit Under Test (UUT)
-   uut: modm_addition PORT MAP (
+   uut: dar_modm_multiplier PORT MAP (
           x => x,
           y => y,
-          m => m,
-          z => z
+          clk => clk,
+          reset => reset,
+          start => start,
+         
+        --  m => m,
+          z => z,
+          done => done
         );
 
-    uut2: modm_adder PORT MAP(
-          x => x,
-          y => y,
-          z => z
-    );
-
-   -- Stimulus process
-   
-   stim_proc: process
-   begin		
-		x <= std_logic_vector(to_unsigned(129, 8));
-		y <= std_logic_vector(to_unsigned(105, 8));		
-		m <= std_logic_vector(to_unsigned(239, 8));
-	
-      wait for 100 ns;	
-		x <= std_logic_vector(to_unsigned(234, 8));
-		y <= std_logic_vector(to_unsigned(238, 8));
-		
-      wait for 100 ns;	
-		x <= std_logic_vector(to_unsigned(215, 8));
-		y <= std_logic_vector(to_unsigned(35, 8));
-      wait;
-      
+ Clk_process :process
+   begin
+		Clk <= '0';
+		wait for Clk_period/2;
+		Clk <= '1';
+		wait for Clk_period/2;
    end process;
+   -- Stimulus process
+  stim_proc: process
+   begin		
+   start<= '0';
+   wait for 50ns;
+   start<='1';
+   
+		x <= std_logic_vector(to_unsigned(2, 8));
+		y <= std_logic_vector(to_unsigned(3, 8));		
+		
+		--m <= std_logic_vector(to_unsigned(239, 8));
+   --   wait for 100 ns;	
+	--	x <= std_logic_vector(to_unsigned(234, 8));
+	--	y <= std_logic_vector(to_unsigned(238, 8));
+    --  wait for 100 ns;	
+	--	x <= std_logic_vector(to_unsigned(215, 8));
+	--	y <= std_logic_vector(to_unsigned(35, 8));
+
+     wait;
+   end process;
+reset <='1' ,'0' after 20ns;
 
 END;
